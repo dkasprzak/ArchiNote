5fc0c666a313be7b1e44787c6c8603fbd974ac8d
dkasprzak <dominik.kasprzak1408@gmail.com>
2025-09-13 12:51:59 +0200

    [backend-tests] Added architecture unit tests

---
+++ ArchiNote.Backend/ArchitectureTests/BaseTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using ArchiNote.Application.Abstractions.Messaging;
+using ArchiNote.Domain.Users;
+using ArchiNote.Infrastructure.Database;
+using Web.Api;
+using Xunit.Abstractions;
+
+namespace ArchitectureTests;
+
+public abstract class BaseTest
+{
+    protected static readonly Assembly DomainAssembly = typeof(User).Assembly;
+    protected static readonly Assembly ApplicationAssembly = typeof(ICommand).Assembly;
+    protected static readonly Assembly InfrastructureAssembly = typeof(ApplicationDbContext).Assembly;
+    protected static readonly Assembly PresentationAssembly = typeof(Program).Assembly;
+}
---
+++ ArchiNote.Backend/ArchitectureTests/Layers/LayerTests.cs
@@ -0,0 +1,74 @@
+﻿using NetArchTest.Rules;
+using Shouldly;
+
+namespace ArchitectureTests.Layers;
+
+public class LayerTests : BaseTest
+{
+    [Fact]
+    public void DomainLayer_Should_NotHaveDependencyOn_ApplicationLayer()
+    {
+        var result = Types.InAssembly(DomainAssembly)
+            .Should()
+            .NotHaveDependencyOn(ApplicationAssembly.GetName().Name)
+            .GetResult();
+        
+        result.IsSuccessful.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void DomainLayer_Should_NotHaveDependencyOn_InfrastructureLayer()
+    {
+        var result = Types.InAssembly(DomainAssembly)
+            .Should()
+            .NotHaveDependencyOn(InfrastructureAssembly.GetName().Name)
+            .GetResult();
+        
+        result.IsSuccessful.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void DomainLayer_Should_NotHaveDependencyOn_PresentationLayer()
+    {
+        var result = Types.InAssembly(DomainAssembly)
+            .Should()
+            .NotHaveDependencyOn(PresentationAssembly.GetName().Name)
+            .GetResult();
+        
+        result.IsSuccessful.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void ApplicationLayer_Should_NotHaveDependencyOn_InfrastructureLayer()
+    {
+        var result = Types.InAssembly(ApplicationAssembly)
+            .Should()
+            .NotHaveDependencyOn(InfrastructureAssembly.GetName().Name)
+            .GetResult();
+        
+        result.IsSuccessful.ShouldBeTrue();
+    }
+    
+    [Fact]
+    public void ApplicationLayer_Should_NotHaveDependencyOn_PresentationLayer()
+    {
+        var result = Types.InAssembly(ApplicationAssembly)
+            .Should()
+            .NotHaveDependencyOn(PresentationAssembly.GetName().Name)
+            .GetResult();
+        
+        result.IsSuccessful.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void InfrastructureLayer_Should_NotHaveDependencyOn_PresentationLayer()
+    {
+        var result = Types.InAssembly(InfrastructureAssembly)
+            .Should()
+            .NotHaveDependencyOn(PresentationAssembly.GetName().Name)
+            .GetResult();
+        
+        result.IsSuccessful.ShouldBeTrue();
+    }
+    
+}
